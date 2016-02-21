ecc0074cd28b902745025c55bfe251171bfd822c
Joeny Bui <joeny.bui@gmail.com>
2016-02-21 02:10:32 -0600

    Added editor update to the template language.

--- src/app/editor/editor.controller.js
+++ src/app/editor/editor.controller.js
@@ -8,20 +8,40 @@
     /* @ngInject */
     function EditorPageController() {
         var vm = this;
+
         vm.testData = ['triangular', 'is', 'great'];
 
         vm.editorOptions = {
-            lineWrapping: true,
+            lineWrapping: false,
             lineNumbers: true,
             //readOnly: 'nocursor',
             //mode: {
             //    name: 'javascript',
             //    json: true
             //},
-            theme:'twilight',
+            theme: 'twilight',
             mode: "javascript",
             indentUnit: 4
             //'text-align': 'left'
-        }
+        };
+
+        vm.name = 'Test Problem';
+        vm.qtype = 0;
+        vm.qtype_options = ['True or False', 'Multiple Choice', 'Fill-in-the-Blank'];
+        vm.stem = {
+            statement: "Tell if the fraction on the left is less or greater than the fraction on the right."
+        };
+
+        vm.keys = {
+            answer: 0,
+            choices: [
+                "2/3",
+                "3/5"
+            ]
+        };
+
+        vm.validation = '';
+        vm.explanation = '';
+        vm.editors = ["joeny"];
     }
 })();
