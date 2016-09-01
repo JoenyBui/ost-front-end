1e5fc3a42033454fd51bb7e8cf2b44c4a4b48949
Joeny Bui <joeny.bui@gmail.com>
2016-08-31 22:41:54 -0500

    Update gulp task.js.

--- gulpfile.js
+++ gulpfile.js
@@ -14,3 +14,13 @@ require('require-dir')('./gulp');
 gulp.task('build', ['clean'], function () {
     gulp.start('buildapp');
 });
+
+gulp.task('serveprod', function() {
+    connect.server({
+        root: ['src'],
+        port: process.env.PORT || 5000, // localhost:5000
+        livereload: false
+    });
+});
+
+gulp.task('default', ['compile']);
